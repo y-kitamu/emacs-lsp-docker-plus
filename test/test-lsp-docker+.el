e337c5f3646b39072591d7ed2a1d5680af3f6ea9
Kitamura <ymyk6602@gmail.com>
2021-05-30 15:54:16 +0900

    add unit tests

--- test/test-lsp-docker+.el
+++ test/test-lsp-docker+.el
@@ -23,11 +23,38 @@
 ;;
 
 ;;; Code:
-
+(require 'cl-lib)
 (require 'ert)
-
 (require 'lsp-docker+)
 (require 'ccls)
+
+
+(defun lsp-docker+-run-on-c++-config (func &rest args)
+  "Run FUNC with lsp-docker+ c++ configuration.
+ARGS is arguments of FUNC."
+  (let ((ldp-foo (getenv "LDP_FOO"))
+        (ldp-bar (getenv "LDP_BAR"))
+        (lsp-docker-command "docker")
+        (lsp-docker-container-name-suffix 0)
+        (lsp-docker+-server-id 'ccls)
+        (lsp-docker+-docker-server-id 'test-docker-lsp)
+        (lsp-docker+-server-command "ccls")
+        (lsp-docker+-image-id "emacslsp/lsp-docker-langservers")
+        (lsp-docker+-docker-options "-u test_user")
+        (lsp-docker+-container-name "lsp-docker")
+        (lsp-docker+-path-mappings (list (cons "${LDP_FOO}/test" "${LDP_BAR}/test")))
+        (lsp-docker+-priority 5)
+        (lsp-docker+-server-cmd-fn #'lsp-docker+-launch-new-container))
+    (unwind-protect
+        (progn
+          (setenv "LDP_FOO" "/home/user")
+          (setenv "LDP_BAR" "/project")
+          (if (null args)
+              (funcall func)
+            (funcall func args)))
+      (setenv "LDP_FOO" ldp-foo)
+      (setenv "LDP_BAR" ldp-bar))))
+
 
 (ert-deftest lsp-docker+-unittest-lsp-docker+-register-client ()
   "Unit test of `lsp-docker+-register-client'."
@@ -39,5 +66,22 @@
       (should (equal 20 (lsp--client-priority client)))
       (should (equal 'test-docker-lsp (lsp--client-server-id client)))
       )))
+
+(ert-deftest lsp-docker+-unittest-lsp-docker+-launch-new-container ()
+  "Unit test of `lsp-docker+-launch-new-container.'"
+  (lsp-docker+-run-on-c++-config
+   (lambda ()
+     (let ((command (lsp-docker+-launch-new-container))
+           (expect '("docker" "run" "--name" "lsp-docker-1" "--rm" "-i" "-v"
+                     "/home/user/test:/project/test"  "-u" "test_user"
+                     "emacslsp/lsp-docker-langservers" "ccls")))
+       (cl-mapcar (lambda (x y) (should (equal x y))) command expect)))))
+
+(ert-deftest lsp-docker+-unittest-lsp-docker+-exec-in-container ()
+  "Unit test of `lsp-docker+-exec-in-container'."
+  (lsp-docker+-run-on-c++-config
+   (lambda ()
+     (let ((command (lsp-docker+-exec-in-container))
+           (expect '("docker" "exec" "-u" "test_user" "-i" "lsp-docker" "ccls")))))))
 
 ;;; test-lsp-docker+.el ends here
